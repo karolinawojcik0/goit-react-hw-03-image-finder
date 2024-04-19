a0855898161a28a5cb48155a2f6799e81ac51b07
Karolina <143939666+karolinawojcik0@users.noreply.github.com>
2024-04-19 16:30:22 +0200

    done

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,18 +1,34 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { ModalWindow, Overlay } from './Modal.css';
 
 export const Modal = ({ isOpen, imageUrl, onClose }) => {
+  useEffect(() => {
+    const handleEscape = event => {
+      if (event.code === 'Escape') {
+        onClose();
+      }
+    };
+
+    if (isOpen) {
+      document.addEventListener('keydown', handleEscape);
+    }
+
+    return () => {
+      document.removeEventListener('keydown', handleEscape);
+    };
+  }, [isOpen, onClose]);
+
   if (!isOpen) return null;
 
   const handleClose = event => {
-    if (event.target === event.currentTarget || event.key === 'Escape') {
+    if (event.target === event.currentTarget) {
       onClose();
     }
   };
 
   return (
     <>
-      <Overlay onClick={handleClose} onKeyDown={handleClose}>
+      <Overlay onClick={handleClose}>
         <ModalWindow>
           <img src={imageUrl} alt="" />
         </ModalWindow>
